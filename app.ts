52aad2daf2faa5faa05aa08ff5c3e059db2aa269
takudzwa17 <takuemujo@gmail.com>
2024-02-21 16:36:16 +0200

    link frontend

--- app.ts
+++ app.ts
@@ -23,7 +23,7 @@ app.use(cookieParser())
 
 // cors => cross origin resource sharing
 app.use(cors({
-    origin: ['http://localhost:3000'],
+    origin: ["elearning-client-ecru.vercel.app"],
     credentials: true,
 }));
 
