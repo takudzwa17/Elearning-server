28852cbea0aa45c23af4ae0347e742d39fe8c170
takudzwa17 <takuemujo@gmail.com>
2024-02-21 14:16:39 +0200

    done

---
+++ routes/course.route.ts
@@ -0,0 +1,78 @@
+import express from 'express';
+import { updateAccessToken } from '../controllers/user.controller';
+import { authorizeRoles, isAuthenticated } from '../middleware/auth';
+import { addAnswer, addQuestion, addReplyToReview, addReview, deleteCourse, editCourse, generateVideoUrl, getAllCourses, getAllCoursesAdmin, getCourseByUser, getSingleCourse, uploadCourse } from '../controllers/course.controller';
+const courseRouter = express.Router();
+
+courseRouter.post(
+    "/create-course",
+    updateAccessToken,
+    isAuthenticated,
+    authorizeRoles("admin"),
+    uploadCourse
+)
+
+courseRouter.put(
+    "/edit-course/:id",
+    updateAccessToken,
+    isAuthenticated,
+    authorizeRoles("admin"),
+    editCourse
+)
+
+courseRouter.get(
+    "/get-course/:id",
+    getSingleCourse
+)
+
+courseRouter.get(
+    "/get-courses",
+    getAllCourses
+)
+
+courseRouter.get(
+    "/get-course-content/:id", updateAccessToken, isAuthenticated,
+    getCourseByUser
+)
+
+courseRouter.put(
+    "/add-question", updateAccessToken, isAuthenticated,
+    addQuestion
+)
+
+courseRouter.put(
+    "/add-answer", updateAccessToken, isAuthenticated,
+    addAnswer
+)
+
+courseRouter.put(
+    "/add-review/:id", updateAccessToken, isAuthenticated,
+    addReview
+)
+
+courseRouter.put(
+    "/add-reply", updateAccessToken, isAuthenticated, authorizeRoles("admin"),
+    addReplyToReview
+)
+
+courseRouter.get(
+    "/get-admin-courses",
+    updateAccessToken,
+    isAuthenticated,
+    authorizeRoles("admin"),
+    getAllCoursesAdmin
+)
+courseRouter.post(
+    "/getVdoCipherOTP",
+    generateVideoUrl
+)
+
+courseRouter.delete(
+    "/delete-course/:id",
+    updateAccessToken,
+    isAuthenticated,
+    authorizeRoles("admin"),
+    deleteCourse
+)
+
+export default courseRouter;
